fcddae56a4fa05c2fd112d29e48c5c179f901436
Andrew George <andrewgeorgeberty@gmail.com>
2022-06-02 00:23:56 +0200

    Coding Challenge #1

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/* 
 // Lecture: Constructor Function and the new Operator
 
 const Person = function (firstName, birthYear) {
@@ -35,6 +35,95 @@ Person.prototype.calcAge = function () {
 };
 
 jonas.calcAge();
+matilda.calcAge();
 
 console.log(jonas.__proto__);
 console.log(jonas.__proto__ === Person.prototype); //true
+
+// - prototype of Person object is the object blueprint that we will create all instances on it.
+
+// - prototype property in Person object is the properties and methods that every instance created from Person object can use.
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+//* Prototype chain
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__.__proto__);
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 3];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+//- adding a new method to prototype of the Array constructor so we can use this method on any array due to prototypal inheritance
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+ */
+
+//=> Coding Challenge #1
+/* 
+Your tasks:
+1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
+'speed' property. The 'speed' property is the current speed of the car in
+km/h
+2. Implement an 'accelerate' method that will increase the car's speed by 10,
+and log the new speed to the console
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+the new speed to the console
+4. Create 2 'Car' objects and experiment with calling 'accelerate' and
+'brake' multiple times on each of them
+Test data:
+§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 2: 'Mercedes' going at 95 km/h 
+*/
+/* 
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const firstCar = new Car('BMW', 120);
+console.log(firstCar);
+
+const secondCar = new Car('Mercedes', 95);
+console.log(secondCar);
+
+// accelerate
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+// brake
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+firstCar.accelerate();
+firstCar.accelerate();
+firstCar.brake();
+
+secondCar.accelerate();
+secondCar.brake();
+secondCar.brake();
+secondCar.brake(); */
